deebc141eb92ba7f91037978db6906b11f1a14db
aerain <j40f893@gmail.com>
2019-01-13 20:30:37 +0900

    Updates

---
+++ precache-manifest.d6d00fc696d9907a0725bf7cfa636ba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5aba999e996f9861011",
+    "url": "/pagetest/static/css/main.fd6069b7.chunk.css"
+  },
+  {
+    "revision": "b5aba999e996f9861011",
+    "url": "/pagetest/static/js/main.b5aba999.chunk.js"
+  },
+  {
+    "revision": "8db9b5bc231a820e909a",
+    "url": "/pagetest/static/js/1.8db9b5bc.chunk.js"
+  },
+  {
+    "revision": "529a30e59e98c540c431",
+    "url": "/pagetest/static/js/runtime~main.529a30e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pagetest/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7a82733154ce141e375dd2fb58f3ec67",
+    "url": "/pagetest/index.html"
+  }
+];
